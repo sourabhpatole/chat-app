5cf63dced57240cdac8ce001eb474f509bccdd85
sourabhpatole <sourabhptole@outlook.com>
2022-04-12 22:36:01 +0530

    create one on one chat api

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -50,5 +50,17 @@ const authUser = expressAsyncHandler(async (req, res) => {
     throw new Error("Fail to create user");
   }
 });
+const allUsers = expressAsyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
 
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser, allUsers };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const chats = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("API is running Successfully");
 });
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
